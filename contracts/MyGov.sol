ae9ffce4c0ea3affe87aabaee3173cb861c695dc
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-11-18 10:15:35 +0300

    Initialize functions

--- contracts/MyGov.sol
+++ contracts/MyGov.sol
@@ -4,6 +4,90 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyGov is ERC20{
     function MyGov(){
-        
+        // TODO: Implement function
+    }
+
+    function delegateVoteTo(){
+        // TODO: Implement function
+    }
+
+    function donateEther(){
+        // TODO: Implement function
+    }
+
+    function donateMyGovToken(){
+        // TODO: Implement function
+    }
+
+    function voteForProjectProposal(){
+        // TODO: Implement function
+    }
+
+    function voteForProjectPayment(){
+        // TODO: Implement function
+    }
+
+    function submitProjectProposal(){
+        // TODO: Implement function
+    }
+
+    function submitSurvey(){
+        // TODO: Implement function
+    }
+
+    function takeSurvey(){
+        // TODO: Implement function
+    }
+
+    function reserveProjectGrant(){
+        // TODO: Implement function
+    }
+
+    function withdrawProjectPayment(){
+        // TODO: Implement function
+    }
+
+    function getSurveyResults(){
+        // TODO: Implement function
+    }
+
+    function getSurveyInfo(){
+        // TODO: Implement function
+    }
+
+    function getSurveyOwner(){
+        // TODO: Implement function
+    }
+
+    function getIsProjectFunded(){
+        // TODO: Implement function
+    }
+
+    function getProjectNextPayment(){
+        // TODO: Implement function
+    }
+
+    function getProjectOwner(){
+        // TODO: Implement function
+    }
+
+    function getProjectInfo(){
+        // TODO: Implement function
+    }
+
+    function getNoOfProjectProposals(){
+        // TODO: Implement function
+    }
+
+    function getNoOfFundedProjects(){
+        // TODO: Implement function
+    }
+
+    function getEtherReceivedByProject(){
+        // TODO: Implement function
+    }
+
+    function getNoOfSurveys(){
+        // TODO: Implement function
     }
 }
