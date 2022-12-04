e8a114a4ff06ae60ad004cb13b6b6d79b3500552
omersuve <omersuve@gmail.com>
2022-12-04 23:45:43 +0300

    import web3

--- test/MyGov.js
+++ test/MyGov.js
@@ -1,9 +1,16 @@
 const MyGov = artifacts.require('MyGovToken')
+const Web3 = require('web3');
 
 contract('MyGov', () => {
     it('should deploy smart contract properly', async () => {
         const myGov = await MyGov.deployed()
         console.log(myGov.address)
         assert(myGov.address !== '')
+    })
+
+    it('should donate eth to the contract owner address', async () => {
+        const myGov = await MyGov.deployed()
+        myGov.donateEther();
+        //assert(myGov.address)
     })
 })
