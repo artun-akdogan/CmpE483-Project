c1ee0e0eb8681077cdd8789b09e9825753b75072
omersuve <omersuve@gmail.com>
2022-12-30 18:52:20 +0300

    functions added

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,28 @@
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = process.env.PRIVATE_KEY;
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
+    fuji: {
+      provider: function () {
+        return new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: `https://avalanche-fuji.infura.io/v3/b5b26cc448bd4bc7b4774b7f7eb96224`,
+          chainId: "0xa869"
+        })
+      },
+      network_id: "*",
+      gas: 3000000,
+      gasPrice: 470000000000,
+      skipDryRun: true
     }
   },
-  mocha: {
-  },
-  compilers: {
-    solc: {
-      version: "0.8.17",
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
-};
+}
