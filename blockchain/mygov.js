fa8de6c9e3540dcb1e4e1765dc53dfbfaa457cd0
omersuve <omersuve@gmail.com>
2023-01-15 15:06:44 +0300

    hotfix bloxberg->fuji

--- blockchain/mygov.js
+++ blockchain/mygov.js
@@ -1,6 +1,6 @@
 import Web3 from "web3";
 
-const provider = new Web3.providers.HttpProvider("https://testnet.bloxberg.org/")
+const provider = new Web3.providers.HttpProvider("https://api.avax-test.network/ext/bc/C/rpc")
 
 const web3 = new Web3(provider)
 
